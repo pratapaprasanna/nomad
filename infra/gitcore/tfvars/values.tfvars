7fea8535654081d26bda0ee4acb7b82fe4a6c961
pratapaprasanna <pratapagoutham@gmail.com>
2022-01-23 13:17:29 +0000

    [Infra] Add infra for github-operations

---
+++ infra/gitcore/tfvars/values.tfvars
@@ -0,0 +1,4 @@
+aws_account_id     = "568533002233"
+region             = "ap-south-1"
+github_user        = "github_ci"
+github_user_policy = "github_ci_user_policy"
---
+++ infra/gitcore/variables.tf
@@ -0,0 +1,11 @@
+variable "github_user" {
+  type = string
+}
+
+variable "github_user_policy" {
+  type = string
+}
+
+variable "aws_account_id"{
+  type = string
+}
